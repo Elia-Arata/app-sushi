57f6895db643ca65ded00fc5fd5ce0d9e43059f9
Elia Arata <elia93.arata@gmail.com>
2023-01-31 17:24:06 +0100

    Fix bootstrap

--- src/components/card.js
+++ src/components/card.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Card extends Component {
   render() {
     return (
-      <div className="col">
-        <div className="card" style={{ width: "18rem", textAligne: "center" }}>
+      <div className="col-4" >
+        <div className="card" style={{textAlign: "center" }}>
         <button onClick = {() => this.props.onIncrement(this.props.card)} className="btn btn-primary">Aggiungi <span className=" badge badge-light">{this.props.card.quantità}</span></button>
           <img src={this.props.card.immagine} className="card-img-top" alt="..." />
           <div className="card-body">
